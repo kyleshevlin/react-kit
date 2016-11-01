73a8403c67c34797ab27a1abaf825d11d4c1802b
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-11-01 19:33:02 +0100

    Add babel with presets

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,12 @@ module.exports = {
   output: {
     path: './build',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   }
 };
