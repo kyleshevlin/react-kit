b8ead08478dbb7a3e342fdc0e30ca10e93684d7f
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-11-01 21:06:22 +0100

    Add webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/entry.js',
   output: {
     path: './build',
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      title: 'react-kit'
+    })
+  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
+    }, {
+      test: /\.ejs$/,
+      loader: 'ejs-loader'
     }]
   }
-};
+}
